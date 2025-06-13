c3893d67676f153ad507fb1ec65ff61e38f243ca
redthing1 <redthing1@alt.icu>
2025-06-12 22:51:28 -0700

    remove legacy import setting

--- binja/__init__.py
+++ binja/__init__.py
@@ -1,5 +1,3 @@
 from .settings import my_settings
 from .interchange import unisymbol_export, unisymbol_import, unixref_import
-
-if my_settings.get_bool("unisymbol.enable_legacy_importers"):
-    from .interchange import ghidra_import, ida_import
+from .interchange import ghidra_import, ida_import
--- binja/settings.py
+++ binja/settings.py
@@ -5,19 +5,6 @@ from binaryninja.settings import Settings
 my_settings = Settings()
 my_settings.register_group("unisymbol", "UniSymbol")
 
-# bool: enable legacy importers (import non-UniSymbol from other tools)
-my_settings.register_setting(
-    "unisymbol.enable_legacy_importers",
-    json.dumps(
-        {
-            "title": "Enable Legacy Importers",
-            "description": "Enable importers for non-UniSymbol data from other tools (e.g. Ghidra, IDA)",
-            "default": False,
-            "type": "boolean",
-            "ignore": ["SettingsProjectScope", "SettingsResourceScope"],
-        }
-    ),
-)
 
 # bool: treat all Ghidra symbols as auto-analysis
 my_settings.register_setting(
